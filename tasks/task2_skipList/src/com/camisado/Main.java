c9abfedc730bb0bc6ebeb877278c1b2ce63f2db1
Anna <anna.khodyreva21@gmail.com>
2018-12-04 11:33:55 +0200

    Add implementation for task 2. Need to add tests

--- tasks/task2_skipList/src/com/camisado/Main.java
+++ tasks/task2_skipList/src/com/camisado/Main.java
@@ -3,6 +3,11 @@ package com.camisado;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SkipList<Integer> list = new SkipList<>();
+        list.add(1);
+        list.add(2);
+        list.add(-2);
+        list.add(20);
+        System.out.println(list);
     }
 }
---
+++ tasks/task2_skipList/tests/com/camisado/SkipListTest.java
@@ -0,0 +1,31 @@
+package com.camisado;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkipListTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+        
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toString() {
+    }
+}
